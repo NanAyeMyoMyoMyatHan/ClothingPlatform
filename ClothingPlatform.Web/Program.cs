365b87d165305b617cac08a89f4afa3544b869ee
NanAyeMyoMyoMyatHan <nanayemyomyomyathan@gmail.com>
2026-07-30 11:09:28 +0630

    fix: unescape password and username when parsing postgresql URI

--- ClothingPlatform.Web/Program.cs
+++ ClothingPlatform.Web/Program.cs
@@ -125,8 +125,8 @@ static string ParseConnectionString(string connectionString)
         {
             var uri = new Uri(connectionString);
             var userInfo = uri.UserInfo.Split(':');
-            var username = userInfo[0];
-            var password = userInfo.Length > 1 ? userInfo[1] : "";
+            var username = Uri.UnescapeDataString(userInfo[0]);
+            var password = userInfo.Length > 1 ? Uri.UnescapeDataString(userInfo[1]) : "";
             var host = uri.Host;
             var port = uri.Port > 0 ? uri.Port : 5432;
             var database = uri.AbsolutePath.TrimStart('/');
